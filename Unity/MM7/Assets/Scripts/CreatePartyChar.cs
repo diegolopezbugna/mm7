2160462e7c83a2f22c5c3cc938429a2b81c563a9
diegolopezbugna <diegolopezbugna@gmail.com>
2017-07-12 17:40:57 -0300

    crate party select portrait

--- Unity/MM7/Assets/Scripts/CreatePartyChar.cs
+++ Unity/MM7/Assets/Scripts/CreatePartyChar.cs
@@ -1,8 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreatePartyChar : MonoBehaviour {
+
+    [SerializeField]
+    private RawImage portraitImage;
+
+    [SerializeField]
+    private int maxPortraits = 20;
+
+    [SerializeField]
+    private int portraitSelected = 1;
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +24,24 @@ public class CreatePartyChar : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void PrevPortraitButtonClick() {
+        if (portraitSelected == 1)
+            portraitSelected = 20;
+        else
+            portraitSelected--;
+        UpdatePortrait();
+    }
+
+    public void NextPortraitButtonClick() {
+        if (portraitSelected == maxPortraits)
+            portraitSelected = 1;
+        else
+            portraitSelected++;
+        UpdatePortrait();
+    }
+
+    private void UpdatePortrait() {
+        portraitImage.texture = Resources.Load(string.Format("Portraits/PC{0:D2}01", portraitSelected)) as Texture;
+    }
 }

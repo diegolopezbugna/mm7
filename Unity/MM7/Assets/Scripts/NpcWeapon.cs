ba22f6dc92385dc51d7d752ca4eeee6d0bf4e81b
diegolopezbugna <diegolopezbugna@gmail.com>
2017-08-23 13:48:06 -0300

    guard added

---
+++ Unity/MM7/Assets/Scripts/NpcWeapon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UMA;
+using UMA.CharacterSystem;
+
+public class NpcWeapon : MonoBehaviour {
+
+    private UMAData umaData;
+
+    [SerializeField]
+    private GameObject weaponPrefab;
+
+	// Use this for initialization
+	void Start () {
+        var umaDynamicAvatar = GetComponent<DynamicCharacterAvatar>();
+        umaData = umaDynamicAvatar.umaData;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCharacterCreated()
+    {
+        LoadRightHand();
+    }
+
+    void LoadRightHand()
+    {
+        var weapon = Instantiate(weaponPrefab);
+        var handGameObject = umaData.GetBoneGameObject("RightHand");
+        weapon.transform.SetParent(handGameObject.transform);
+        weapon.transform.localPosition = new Vector3(-0.09f, 0.04f, -0.02f);
+        weapon.transform.localRotation = Quaternion.identity;
+    }
+
+}

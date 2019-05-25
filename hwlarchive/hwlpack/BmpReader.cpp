41c55e81f8d5facb8b48930cb78a6408072a2813
diegolopezbugna <diegolopezbugna@gmail.com>
2019-05-25 03:29:47 -0300

    water transparency fixed, 32 bits with alpha support added

--- hwlarchive/hwlpack/BmpReader.cpp
+++ hwlarchive/hwlpack/BmpReader.cpp
@@ -1,58 +1,6 @@
 #include "stdafx.h"
 
-uint16_t *BmpReader::Read16BitsFile(std::string fileName, uint32_t &outWidth, uint32_t &outHeight) {
-
-	FILE *pFile = fopen(fileName.c_str(), "rb");
-
-	BITMAPFILEHEADER header1;
-	BITMAPINFOHEADER header2; // TODO: another bmp version?
-
-	fread(&header1, sizeof(BITMAPFILEHEADER), 1, pFile);
-	fread(&header2, sizeof(BITMAPINFOHEADER), 1, pFile);
-
-	fseek(pFile, header1.bfOffBits, SEEK_SET);  // just in case
-
-	uint16_t *pReversedPixels = new uint16_t[header2.biWidth * header2.biHeight]; // 16 bits
-
-	fread(pReversedPixels, 2, header2.biWidth * header2.biHeight, pFile);
-
-	fclose(pFile);
-
-	uint16_t *pPixels = new uint16_t[header2.biWidth * header2.biHeight]; // 16 bits
-
-	// TODO: tiene que ser multiplo de 4
-	for (int h = 0; h < header2.biHeight; h++) {
-		for (int w = 0; w < header2.biWidth; w++) {
-			pPixels[(511 - h) * header2.biWidth + w] = pReversedPixels[h * header2.biWidth + w];
-		}
-	}
-
-	outWidth = header2.biWidth;
-	outHeight = header2.biHeight;
-
-	return pPixels;
-}
-
-uint8_t *BmpReader::Read24BitsFile(std::string fileName) {
-
-	FILE *pFile = fopen(fileName.c_str(), "rb");
-
-	BITMAPFILEHEADER header1;
-	BITMAPV4HEADER header2;
-
-	fread(&header1, sizeof(BITMAPFILEHEADER), 1, pFile);
-	fread(&header2, sizeof(BITMAPV4HEADER), 1, pFile);
-
-	uint8_t *pPixels = new uint8_t[header2.bV4Width * header2.bV4Height * 3]; // 24 bits
-
-	fread(pPixels, 3, header2.bV4Width * header2.bV4Height, pFile);
-
-	fclose(pFile);
-
-	return pPixels;
-}
-
-uint16_t *BmpReader::Read24BitsFileTo16Bits(std::string fileName, uint32_t &outWidth, uint32_t &outHeight) {
+uint16_t *BmpReader::Read24or32BitsFileTo16Bits(std::string fileName, uint32_t &outWidth, uint32_t &outHeight) {
 
 	FILE *pFile = fopen(fileName.c_str(), "rb");
 	if (pFile == nullptr) {
@@ -64,41 +12,41 @@ uint16_t *BmpReader::Read24BitsFileTo16Bits(std::string fileName, uint32_t &outW
 
 	fread(&header1, sizeof(BITMAPFILEHEADER), 1, pFile);
 	fread(&header2, sizeof(BITMAPINFOHEADER), 1, pFile);
+	unsigned short bytesPerPixel = header2.biBitCount == 32 ? 4 : 3;
 
 	fseek(pFile, header1.bfOffBits, SEEK_SET);  // just in case
 
-	uint8_t *pPixels24 = new uint8_t[header2.biWidth * header2.biHeight * 3]; // 24 bits
+	uint8_t *pPixels = new uint8_t[header2.biWidth * header2.biHeight * bytesPerPixel];
 
-	fread(pPixels24, 3, header2.biWidth * header2.biHeight, pFile);
+	fread(pPixels, bytesPerPixel, header2.biWidth * header2.biHeight, pFile);
 
 	fclose(pFile);
 
-	uint16_t *pPixels = new uint16_t[header2.biWidth * header2.biHeight]; // 16 bits
+	uint16_t *pPixels16 = new uint16_t[header2.biWidth * header2.biHeight]; // 16 bits
 
 	// TODO: tiene que ser multiplo de 4
 	for (int h = 0; h < header2.biHeight; h++) {
 		for (int w = 0; w < header2.biWidth; w++) {
 
-			//pPixels[(511 - h) * header2.biWidth + w] = pReversedPixels[h * header2.biWidth + w];
+			//555 format, each color is from 0 to 32, instead of 0 to 256
+			//int p24i = h * width_in_bytes_32 + w * 4;
+			//int p16i = h * width_in_bytes_16 + w * 2;
+			int src_i = (h * header2.biWidth + w) * bytesPerPixel; // TODO: padding???
+			uint16_t red = (uint16_t)(pPixels[src_i + 0] * 31.f / 255.f);
+			uint16_t grn = (uint16_t)(pPixels[src_i + 1] * 31.f / 255.f);
+			uint16_t blu = (uint16_t)(pPixels[src_i + 2] * 31.f / 255.f);
+			uint16_t alpha = header2.biBitCount == 32 ? (uint16_t)(pPixels[src_i + 3]) : 0;
+			uint16_t sum = (red) | (grn << 5) | (blu << 10) | (alpha << 15);
 
-            //555 format, each color is from 0 to 32, instead of 0 to 256
-            //int p24i = h * width_in_bytes_32 + w * 4;
-            //int p16i = h * width_in_bytes_16 + w * 2;
-            int src_i = (h * header2.biWidth + w) * 3; // TODO: padding???
-			uint16_t red = (uint16_t)(pPixels24[src_i + 0] * 31.f / 255.f);
-			uint16_t grn = (uint16_t)(pPixels24[src_i + 1] * 31.f / 255.f);
-			uint16_t blu = (uint16_t)(pPixels24[src_i + 2] * 31.f / 255.f);
-            uint16_t sum = (red) | (grn << 5) | (blu << 10);
-
-            int dest_i = ((header2.biHeight - 1 - h) * header2.biWidth + w); // * 2;
-            memcpy(&pPixels[dest_i], &sum, 2);
+			int dest_i = ((header2.biHeight - 1 - h) * header2.biWidth + w); // * 2;
+			memcpy(&pPixels16[dest_i], &sum, 2);
 		}
 	}
 
 	outWidth = header2.biWidth;
 	outHeight = header2.biHeight;
 
-	return pPixels;
+	return pPixels16;
 }
 
 
--- hwlarchive/hwlpack/hwlpack.cpp
+++ hwlarchive/hwlpack/hwlpack.cpp
@@ -8,45 +8,47 @@
 #define HWL_TEX_GMASK  0x03E0
 #define HWL_TEX_BMASK  0x001F
 
+const char* dataFolder = "C:\\Program Files (x86)\\3DO\\Might and Magic VII\\DATA\\";
+const char* texturesFolder = "E:\\MM7\\converted\\";
+BmpReader bmpReader;
+
+int updateTexture(HWLContainer* pD3DBitmaps, std::string textureName) {
+	uint32_t width = 0;
+	uint32_t height = 0;
+
+	auto bmpFilename = std::string(texturesFolder) + textureName + ".bmp";
+	uint16_t *pixels = bmpReader.Read24or32BitsFileTo16Bits(bmpFilename, width, height);
+
+	if (pixels == nullptr) {
+		printf(" error reading %s\n", bmpFilename.c_str());
+		return -1;
+	}
+
+	zlib::uLong compressedSize = zlib::compressBound(width * height * 2);
+	uint8_t *compressedPixels = (uint8_t *)malloc(compressedSize);
+
+	auto result = zlib::compress(compressedPixels, &compressedSize, (uint8_t *)pixels, width * height * 2);
+	if (result != Z_OK) {
+		printf(" error compressing %s -> result: %i\n", textureName, result);
+		return -1;
+	}
+
+	pD3DBitmaps->UpdateTexture(textureName, width, height, compressedSize, (uint16_t *)compressedPixels);
+
+	delete[] pixels;
+	free(compressedPixels);
+}
 
 int main()
 {
-	const char* dataFolder = "C:\\Program Files (x86)\\3DO\\Might and Magic VII\\DATA\\";
-	const char* texturesFolder = "E:\\MM7\\converted\\";
-
 	printf("Starting...\n");
 
 	HWLContainer* pD3DBitmaps = new HWLContainer();
 	pD3DBitmaps->Open(std::string(dataFolder) + "d3dbitmap.hwl");
-	auto kk = std::string(dataFolder) + std::string(dataFolder);
+
 	auto textureNames = pD3DBitmaps->GetAllTextureNames();
-
-	for (auto textureName: textureNames) {
-
-		BmpReader bmpReader = BmpReader();
-		uint32_t width = 0;
-		uint32_t height = 0;
-
-		auto bmpFilename = std::string(texturesFolder) + textureName + ".bmp";
-		uint16_t *pixels = bmpReader.Read24BitsFileTo16Bits(bmpFilename, width, height);
-		if (pixels == nullptr) {
-			printf(" error reading %s\n", bmpFilename.c_str());
-			continue;
-		}
-
-		zlib::uLong compressedSize = zlib::compressBound(width * height * 2);
-		uint8_t *compressedPixels = (uint8_t *)malloc(compressedSize);
-
-		auto result = zlib::compress(compressedPixels, &compressedSize, (uint8_t *)pixels, width * height * 2);
-		if (result != Z_OK) {
-			printf(" error compressing %s -> result: %i\n", textureName, result);
-			continue;
-		}
-
-		pD3DBitmaps->UpdateTexture(textureName, width, height, compressedSize, (uint16_t *)compressedPixels);
-
-		delete[] pixels;
-		free(compressedPixels);
+	for (auto textureName : textureNames) {
+		updateTexture(pD3DBitmaps, textureName);
 	}
 
 	delete pD3DBitmaps;
